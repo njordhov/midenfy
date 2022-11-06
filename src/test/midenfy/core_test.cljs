43c0be890786b19d203b323c6370e9440e7ed227
Terje Norderhaug <terje@in-progress.com>
2022-11-05 17:09:11 -0700

    Enable test

---
+++ src/test/midenfy/core_test.cljs
@@ -0,0 +1,10 @@
+(ns midenfy.core-test
+  (:require 
+   [cljs.test :refer (deftest is)]
+   [clojure.string :as string]
+   [shadow.resource :as rc]))
+   
+(deftest positive-test
+  (is true))
+
+
